490a5f39b5ff775eaa742b2de90a38ee76d8f762
Muhammad Abdul Rahman <68278872+chrahman@users.noreply.github.com>
2024-03-17 15:35:59 +0500

    changed bd config

--- src/config/db/common.config.ts
+++ src/config/db/common.config.ts
@@ -12,13 +12,17 @@ export default function commonConfig(): DataSourceOptions {
   };
 
   const prodConfig = {
-    url: process.env.DATABASE_URL,
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     ssl: { rejectUnauthorized: false },
   };
 
   return {
     type: 'postgres',
-    synchronize: false,
+    synchronize: true,
     entities: [join(__dirname, '../../**/*.entity{.ts,.js}')],
     ...(process.env.NODE_ENV === 'production' ? prodConfig : devConfig),
   };
