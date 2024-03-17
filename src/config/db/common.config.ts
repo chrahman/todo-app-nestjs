eee64567df33113008e2ef86d5d4c4912fd75f7b
Muhammad Abdul Rahman <68278872+chrahman@users.noreply.github.com>
2024-03-17 15:52:21 +0500

    revert db config

--- src/config/db/common.config.ts
+++ src/config/db/common.config.ts
@@ -12,11 +12,7 @@ export default function commonConfig(): DataSourceOptions {
   };
 
   const prodConfig = {
-    host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT, 10),
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    url: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
   };
 
